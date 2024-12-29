b99fdb8c30ba0530823171afe0bf42bb8fc2908b
jajsfw <c@cereals.fun>
2024-12-29 20:48:41 +0700

    dome 0.0.6.9..

--- next.config.js
+++ next.config.js
@@ -1,8 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: ['@octokit/rest'],
   images: {
     domains: ['avatars.githubusercontent.com'],
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        'undici': false,
+      };
+    }
+    return config;
   },
 }
 
