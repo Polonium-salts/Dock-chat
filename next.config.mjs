8b5db6f6b200335e557ebb8beaefdc02473d6a12
Polonium-salts <c@cereals.fun>
2025-02-03 07:52:09 +0800

    dome

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // 启用样式优化
+  optimizeFonts: true,
+  // 确保在生产环境中正确加载样式
+  poweredByHeader: false,
+  reactStrictMode: true,
+  swcMinify: true,
+}
 
 export default nextConfig;
